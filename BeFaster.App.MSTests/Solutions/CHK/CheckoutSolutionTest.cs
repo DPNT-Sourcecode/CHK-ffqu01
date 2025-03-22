260971aacc7a07d9983cd557c64c7108f40d332a
sbx_user1051 <sbx_user1051@169.254.15.39>
2025-03-22 17:41:43 +0000

    Sat Mar 22 17:41:43 UTC 2025

---
+++ BeFaster.App.MSTests/Solutions/CHK/CheckoutSolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeFaster.App.Solutions.CHK;
+
+namespace BeFaster.App.MSTests.Solutions.CHK
+{
+    [TestClass]
+    public class CheckoutSolutionTest
+    {
+        [TestMethod]
+        public void CheckOut() 
+        {
+            
+        }
+    }
+}
--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -8,12 +8,13 @@ namespace BeFaster.App.Solutions.CHK
         /// Calculates the total price of all the items in the basket.
         /// </summary>
         /// <param name="skus">String containing list of items in the basket.</param>
-        /// <returns>Int total price.</returns>
-        /// <exception cref="SolutionNotImplementedException"></exception>
+        /// <returns>Int total price or -1 for illegal input.</returns>
         public static int ComputePrice(string? skus)
         {
-            
+
+            return -1;
         }
     }
 }
+
 
