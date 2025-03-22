d66d5b5b2c1d4a23022b7112a274046854c16c7e
sbx_user1051 <sbx_user1051@169.254.64.89>
2025-03-22 11:39:08 +0000

    Sat Mar 22 11:39:08 UTC 2025

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -6,6 +6,10 @@ namespace BeFaster.App.Solutions.SUM
     {
         public static int Sum(int x, int y)
         {
+            if (x <= 0 || y <= 0) 
+            {
+                throw new InvalidParamException("Inputs must be a positive integer between 0-100");
+            }
             return x + y;
         }
     }
@@ -13,7 +17,10 @@ namespace BeFaster.App.Solutions.SUM
     public class InvalidParamException : Exception
     {
         public InvalidParamException() { }
+
+        public InvalidParamException(string message) : base(message) { }
     }   
 }
+
 
 
