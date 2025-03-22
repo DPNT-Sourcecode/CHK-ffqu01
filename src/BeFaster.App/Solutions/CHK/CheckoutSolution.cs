c79bcb7c9aa385dba97637e427201770e879d11d
sbx_user1051 <sbx_user1051@169.254.15.39>
2025-03-22 17:38:43 +0000

    Sat Mar 22 17:38:43 UTC 2025

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -4,9 +4,16 @@ namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
+        /// <summary>
+        /// Calculates the total price of all the items in the basket.
+        /// </summary>
+        /// <param name="skus">String containing list of items in the basket.</param>
+        /// <returns>Int total price.</returns>
+        /// <exception cref="SolutionNotImplementedException"></exception>
         public static int ComputePrice(string? skus)
         {
-            throw new SolutionNotImplementedException();
+            
         }
     }
 }
+
