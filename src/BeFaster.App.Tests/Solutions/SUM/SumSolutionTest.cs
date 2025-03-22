606d24b3f2ae5032a471ed5ba58264ae289bb8e5
sbx_user1051 <sbx_user1051@169.254.15.39>
2025-03-22 16:17:31 +0000

    Sat Mar 22 16:17:31 UTC 2025

--- src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -1,24 +1,25 @@
-﻿//using BeFaster.App.Solutions.SUM;
-//using NUnit.Framework;
+﻿using BeFaster.App.Solutions.SUM;
+using NUnit.Framework;
 
-//namespace BeFaster.App.Tests.Solutions.SUM
-//{
-//    [TestFixture]
-//    public class SumSolutionTest
-//    {
-//        [TestCase(1, 1, ExpectedResult = 2)]
-//        public int ComputeSum(int x, int y)
-//        {
-//            return SumSolution.Sum(x, y);
-//        }
+namespace BeFaster.App.Tests.Solutions.SUM
+{
+    [TestFixture]
+    public class SumSolutionTest
+    {
+        [TestCase(1, 1, ExpectedResult = 2)]
+        public int ComputeSum(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
 
-//        [TestCase(-1,1)]
-//        public void negativeParameter(int x, int y)
-//        {
-//            Assert.Throws<InvalidParamException>(SumSolution.Sum(x, y));
-//        }
+        [TestCase(-1, 1)]
+        public void negativeParameter(int x, int y)
+        {
+            Assert.Throws<InvalidParamException>(SumSolution.Sum(x, y));
+        }
 
-//        //I'm going to switch to using the built in unit testing library as it is what i am more familiar with :)
-//    }
-//}
+        //I'm going to switch to using the built in unit testing library as it is what i am more familiar with :)
+    }
+}
+
 
