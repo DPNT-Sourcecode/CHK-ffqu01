eee52811d31f7fc1c529a373c994150091198713
sbx_user1051 <sbx_user1051@169.254.15.39>
2025-03-22 16:47:32 +0000

    Sat Mar 22 16:47:32 UTC 2025

--- BeFaster.App.MSTests/SumSolutionTest.cs
+++ BeFaster.App.MSTests/SumSolutionTest.cs
@@ -11,12 +11,16 @@ namespace BeFaster.App.MSTests
             Assert.AreEqual(2, SumSolution.Sum(1, 1));
         }
         [TestMethod]
-        public void TestNegativeInput() 
+        public void TestNegativeInput()
         {
             Assert.ThrowsException<InvalidParamException>(() => SumSolution.Sum(-1, 1));
+            Assert.ThrowsException<InvalidParamException>(() => SumSolution.Sum(1, -1));
         }
         [TestMethod]
-        public void TestAboveMaxInput() { 
-            
+        public void TestAboveMaxInput()
+        {
+            Assert.ThrowsException<InvalidParamException>(() => SumSolution.Sum(101, 1));
+            Assert.ThrowsException<InvalidParamException>(() => SumSolution.Sum(1, 101));
         }
+    }
 }
--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -4,9 +4,11 @@ namespace BeFaster.App.Solutions.SUM
 {
     public static class SumSolution
     {
+        private const int SUMMAX = 100;
+        private const int SUMMIN = 0;
         public static int Sum(int x, int y)
         {
-            if (x <= 0 || y <= 0) 
+            if (x <= 0 || y <= 0 || x > 100 || y > 100) 
             {
                 throw new InvalidParamException("Inputs must be a positive integer between 0-100");
             }
@@ -21,3 +23,4 @@ namespace BeFaster.App.Solutions.SUM
         public InvalidParamException(string message) : base(message) { }
     }   
 }
+
